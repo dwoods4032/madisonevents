704e56dd6a85255761436bdd74eea4f6d268a2ff
dwoods4032 <Dwoods54@yahoo.com>
2025-04-17 20:41:23 -0400

    Update route.ts

--- app/api/isthmus/route.ts
+++ app/api/isthmus/route.ts
@@ -1,12 +1,18 @@
 // app/api/isthmus/route.ts
+import { NextResponse } from "next/server";
 
 export async function GET() {
-  const response = await fetch("https://isthmus.com/api/rss/all-events/");
-  const xml = await response.text();
-
-  return new Response(xml, {
+  const res = await fetch("https://isthmus.com/api/rss/events", {
     headers: {
-      "Content-Type": "application/xml",
-    },
+      "Content-Type": "application/xml"
+    }
+  });
+
+  const data = await res.text();
+  return new NextResponse(data, {
+    status: 200,
+    headers: {
+      "Content-Type": "application/xml"
+    }
   });
 }
